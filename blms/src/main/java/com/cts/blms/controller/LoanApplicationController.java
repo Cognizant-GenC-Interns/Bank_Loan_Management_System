076eac07444916f76cf6176d24cc2a15bdcef5a7
Rithanya <2401151@cognizant.com>
2025-05-15 18:05:25 +0530

    Repayment -M4 Model file

--- blms/src/main/java/com/cts/blms/controller/LoanApplicationController.java
+++ blms/src/main/java/com/cts/blms/controller/LoanApplicationController.java
@@ -1,8 +1,9 @@
 package com.cts.blms.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+
+@Controller
 public class LoanApplicationController {
 
 }
--- blms/src/main/java/com/cts/blms/model/Repayment.java
+++ blms/src/main/java/com/cts/blms/model/Repayment.java
@@ -1,12 +1,39 @@
 package com.cts.blms.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Date;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "repayment")
+@Table(name = "Repayment")
 public class Repayment {
-
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+	 @Column(name = "repayment_id")
+	 private int repaymentId;
+	 
+	 @JoinColumn(name = "application_id", referencedColumnName = "applicationId", nullable = false)
+	 @Column(name="application_id")
+	 private int applicationId;
+	 
+	 @Column(name="due_date")
+	 private Date dueDate;
+	 
+	 @Column(name="amount_due")
+	 private double amountDue;
+	 
+	 @Column(name="payment_date")
+	 private Date paymentDate;
+	 
+	 @Column(name="payment_status")
+	 private PaymentStatus paymentStatus;
+	 
+	
 }
