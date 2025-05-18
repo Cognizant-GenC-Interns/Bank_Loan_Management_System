f8ed8f3ecb9f9b676d4a50d4845015f996db100c
2400743 <2400743@LTIN604019.cts.com>
2025-05-18 23:10:09 +0530

    model LoanProduct is updated

--- blms/src/main/java/com/cts/blms/model/LoanProduct.java
+++ blms/src/main/java/com/cts/blms/model/LoanProduct.java
@@ -1,12 +1,29 @@
 package com.cts.blms.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "loanproduct")
 public class LoanProduct {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer loanProductId;
+	
+	@Column(nullable = false, length = 100)
+    private String ProductName;
+	
+	@Column(nullable = false)
+    private Double interestRate;
 
+    @Column(nullable = false)
+    private Double minAmount;
+
+    @Column(nullable = false)
+    private Double maxAmount;
+
+    @Column(nullable = false)
+    private Integer tenure;
 }
