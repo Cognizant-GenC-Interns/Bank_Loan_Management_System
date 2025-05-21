e24c110f468a90334852171a4f5c3121bd440497
kaviya <2401096@cognizant.com>
2025-05-21 16:07:32 +0530

    login,sing up,edit profile completed in module one

--- blms/src/main/java/com/cts/blms/service/LoanProductService.java
+++ blms/src/main/java/com/cts/blms/service/LoanProductService.java
@@ -1,11 +1,11 @@
-package com.cts.blms.service;
-
-import java.util.List;
-
-import com.cts.blms.model.LoanProduct;
-
-public interface LoanProductService {
-	public LoanProduct addLoanProduct(LoanProduct loan);
-	public LoanProduct updateLoanProduct(Integer loanProductId,LoanProduct loan);
-	public List<LoanProduct> getLoanProductDetails();
-}
+//package com.cts.blms.service;
+//
+//import java.util.List;
+//
+//import com.cts.blms.model.LoanProduct;
+//
+//public interface LoanProductService {
+//	public LoanProduct addLoanProduct(LoanProduct loan);
+//	public LoanProduct updateLoanProduct(Integer loanProductId,LoanProduct loan);
+//	public List<LoanProduct> getLoanProductDetails();
+//}
